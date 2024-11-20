850645961a6ee5f7540877e6b9c668eed93a274f
kpzo <153002207+kpzo@users.noreply.github.com>
2024-11-20 06:19:21 -0500

    Week3 Express Tutorial

---
+++ 02-express-tutorial/homework-practice/app02.js
@@ -0,0 +1,25 @@
+const app = require('express')();
+
+app.get('/', (req, res) => {
+    console.log('User hit the resource')
+    res.status(200).send('Home Page')
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page')
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>resource not found booo</h1>')
+});
+
+app.listen(3000, () => {
+    console.log('server is listening on port 3000....')
+})
+
+// app.get
+// app.post
+// app.put
+// app.patch
+// app.delete
+    
